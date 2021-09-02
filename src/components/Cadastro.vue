ab759bd688630014582a2353c30fd4704ea88a09
Matheus Santos <matheus.santos@sprintb.com>
2021-09-02 16:18:39 -0300

    'quizzFinalizado'

---
+++ src/components/Cadastro.vue
@@ -0,0 +1,39 @@
+<template>
+    <div id="root">
+
+  <input v-model="newCat">
+  <button v-on:click="addKitty">
+    + ADD
+  </button>
+  <ul>
+    <li v-for="cat in cats" :key="cat">{{ cat.name }}</li>
+      
+    
+  </ul>
+</div>
+</template>
+
+<script>
+
+import { defineComponent } from '@vue/composition-api'
+
+export default ({
+        new: Vue ({
+        el: '#root',
+        data: {
+        cats: [
+            {name: 'kitkat'},
+            {name: 'fish'},
+            {name: 'henry'}
+            
+        ],
+        newCat: ''
+    },
+    methods: {
+        addKitty: function() {
+        return this.cats.push({name: this.newCat})
+        }
+    }
+    })
+});
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+         <b-nav tabs>
+            <b-nav-item disabled><b>Damdam Quizz App</b></b-nav-item>
+            <b-nav-item disabled>Respostas: {{numCorrect}}/{{numTotal}}</b-nav-item>
+            <b-nav-item disabled>Dropdown</b-nav-item>
+            <b-nav-item-dropdown>
+                <b-nav-item>Click to Smille :D</b-nav-item>
+                <b-nav-item>Love u</b-nav-item>
+            </b-nav-item-dropdown>
+            
+        </b-nav>
+    </div>
+</template>
+
+<script>
+
+export default ({
+    props: [
+        'numCorrect',
+        'numTotal'
+    ]
+})
+</script>
